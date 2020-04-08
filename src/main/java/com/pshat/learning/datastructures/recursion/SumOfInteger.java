fbd3455682cdd45d0a57c3e82764f8b9fabb7ce3
admin <admin@10.91.169.196>
2020-04-08 21:10:06 +0530

    Jospehus, Palindrome and Sum of integer using recursion

---
+++ src/main/java/com/pshat/learning/datastructures/recursion/SumOfInteger.java
@@ -0,0 +1,20 @@
+package com.pshat.learning.datastructures.recursion;
+
+public class SumOfInteger {
+	
+	/*
+	 * This method is implemented using tail recursion. here we could have avoided sum parameter and added in the program, but instead
+	 * we implemented it using tail recursion, which has a better performance comparatively
+	 */
+	private static int sumNumber(int number,int sum) {
+		if(number < 10)
+			return sum + number;
+		
+		return sumNumber(number/10, sum + (number % 10));
+	}
+	
+	public static void main(String[] args) {
+		int number = 55555555;
+		System.out.println(sumNumber(number, 0));
+	}
+}
